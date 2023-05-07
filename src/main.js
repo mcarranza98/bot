8bc9abf7a581d33d50632e99c2cd509ec30d5014
mcarranza98 <miguelcarranza207@gmail.com>
2023-05-06 19:41:01 -0600

    Bot funcional

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-const { app, BrowserWindow, BrowserView, session, ipcMain } = require('electron');
+const { app, BrowserWindow, BrowserView, session, ipcMain, dialog, webContents } = require('electron');
 const puppeteer = require('puppeteer-core');
 const pie = require('puppeteer-in-electron');
-const { Client } = require('whatsapp-web-electron.js');
+const { Client, LocalAuth, MessageMedia } = require('tecsom-whatsapp-web.js');
 const path = require('path');
 //const sqlite3 = require('sqlite3').verbose();
 const server = require('../app');
@@ -11,69 +11,56 @@ const natural = require('natural');
 const { v4: uuidv4 } = require('uuid');
 const request = require("request");
 
+const {PosPrinter} = require("electron-pos-printer");
+
 const distance = require('google-distance-matrix');
 distance.key('AIzaSyDH9nP0KFFro41ufDHWLSTAHp9Rxa__ofc');
 
 const NodeGeocoder = require('node-geocoder');
 
-const { MessageMedia } = require('whatsapp-web-electron.js');
-const { getPost, getImage } = require('random-reddit')
+const minutes4reset = 120 
+const CryptoJS = require('crypto-js');
+const { start } = require('repl');
+const { contextIsolated } = require('process');
+var admitedUser = true
 
-const test = true;
+const initialTimestamp = Math.floor(Date.now() / 1000);
 
 const createApp = async () => {
 
     const { browser, window } = await createMainWindow(app);
+
+    //window.removeMenu()
+
+
     
     const { client, view } = await createWhatsappView(window, browser);
 
+    //const intervalID = setInterval(checkAutocompleteOrder, 5000, window, client); //corre cada 5 segundo para comprobar ordenes pendientes y autocompletarlas
+
+
+   
+
+    attachView_Whatsapp(client,window,view,browser);
+
     const CHANNEL_NAME = 'main';
 
-    ipcMain.on(CHANNEL_NAME, (event, data) => {
+    ipcMain.on('PRINT_CHANNEL', (event, data) => {
 
-        if(data == "toggle-wha"){
+        const db_order = new Database(path.resolve(__dirname, '..', 'database' , 'orders.db'));
+        let command = db_order.prepare('SELECT * FROM orders WHERE telefono = ? AND timestamp = ?' );
+        const orders = command.all(data.telefono, data.timestamp);
 
-            const views = window.getBrowserView();
+        let comando2 = db_order.prepare('SELECT * FROM order_data WHERE telefono = ? AND timestamp = ?' );
+        const order_data = comando2.get(data.telefono, data.timestamp);
 
-            if(!views){
+        const db_setings = new Database(path.resolve(__dirname,'..','database' , 'settings.db'));
 
-                window.setBrowserView(view);
-
-                const size = window.getContentSize();
-
-                const bounds = window.getBounds();
-
-                const coordy = bounds.height - size[1];
-
-                view.setBounds({ x: 0, y: coordy, width: bounds.width/2 , height: size[1]});
-
-                view.setAutoResize({
-                    width: true,
-                    height: true,
-                });
-
-                window.addBrowserView(view);
-
-            }else{
-
-                window.removeBrowserView(view);
-
-            }
-        }
-        else if(data == "close-wha"){
-
-            const views = window.getBrowserView();
-
-            if(views){
-
-                window.removeBrowserView(view);
-                
-            }
-
-        }
+        printOrder(order_data, orders, db_setings)
 
     });
 
+    // COMMENT TEST
     app.on('resize', function(e,x,y){
 
         window.setSize(x, y);
@@ -94,39 +81,121 @@ const createApp = async () => {
             createMainWindow(app);
         }
     });
-
-   
+    
 };
 
-const createWhatsappView = async(window, browser) => {
+function isClientConnected(client){
+    if(!client.pupPage) {
+        console.log("no inicializado")
+    }else{
+        console.log("inicializado")
+    }
+    
+    
+}
+
+
+
+function attachView_Whatsapp(client,window){
+
+    ipcMain.removeAllListeners(["main"]);
+    ipcMain.removeAllListeners(["SEND_WHA"]);
+    ipcMain.removeAllListeners(["RELOAD_WHATSAPP"]);
+
+    ipcMain.on('RELOAD_WHATSAPP', async (event, data) => {
+        
+        client.logout()
+
+    })
+
+
+    ipcMain.on("main", (event, data) => {
+
+       
+        if(data == "print"){
+
+            var res = {};
+            res.message = "printers array";
+            res.printers = window.webContents.getPrinters()
+
+            window.webContents.send('main', res);
+
+        }
+
+    });
+
+
+    ipcMain.on("LOAD_PRINTERS",async function(){
+
+        const printers = await window.webContents.getPrintersAsync();
+        window.webContents.send('LOAD_PRINTERS', printers);
+
+    })
+
+}
+
+
+const createWhatsappView = async (window) => {
+
+    console.log("view");
 
     const view = new BrowserView();
-
-    const post = await getImage('ratemysetup');
-
-    const media = await MessageMedia.fromUrl(post); 
 
     await window.webContents.session.clearStorageData();
     await window.webContents.session.clearCache();
 
-    const client = new Client( browser, view );
+    const client = new Client({
+        authStrategy: new LocalAuth()
+    });
+    
     
     client.on('ready', () => { 
 
-        window.webContents.send('main', "whatsapp-connected");
+        isClientConnected(client)
+        //window.webContents.send('main', "whatsapp-connected");
+        window.webContents.send('W_CONNECTION', {connection:"connected"});
+
+
 
     });
 
+    client.on('qr', (qr) => {
+        // Generate and scan this code with your phone
+        console.log({qr})
+        window.webContents.send('W_QR_UPDATES', {qr});
+
+    });
+
+    client.on('disconnected', async () => { 
+
+        console.log("disconnected");
+
+        window.webContents.send('W_CONNECTION', {connection:"disconnected"});
+        await createWhatsappView(window);
+
+    });
+
+    client.on('change_state', (state) => { 
+
+        console.log(state);
+      
+
+    });
+
+    
+
+    
     client.on('message', msg => {
 
+        console.log( {msg} );
         if(msg.from == "status@broadcast" || msg.from.length > 18) return;
 
         if (msg.from == "5218712662748@c.us" || msg.from == "5218717978267@c.us"  || msg.from == "5218713427215@c.us" ){
 
-            console.log( {post} );
+            
+            console.log('mandar mensaje');
 
-
-            client.sendMessage(msg.from, media);
+            client.sendMessage(msg.from, 'Este es un mensaje de prueba para comprobar que si funciona el wats');
 
             db_conversaciones.close();
             db_main.close();
@@ -137,10 +206,9 @@ const createWhatsappView = async(window, browser) => {
 
     client.initialize();
 
-    return { view : view, client: client };
+    return {client}
 
 }
-
 
 const createMainWindow = async (app) => {
 
@@ -154,13 +222,16 @@ const createMainWindow = async (app) => {
         height: 700,
         webPreferences:{
             nodeIntegration : true,
-            preload: path.join(__dirname, '..' , 'public', 'assets' , 'scripts' , 'inicio' , 'renderer.js'),
+            preload: path.resolve(__dirname, '..', 'public', 'assets' , 'scripts' , 'inicio' , 'renderer.js'),
             contextIsolation: true,
         },
         useContentSize: true,
         show: false,
+        icon: path.resolve(__dirname, '..', 'public', 'assets' , 'images' , 'icons' , 'tecsomlogowhite.ico')
 
     });
+
+    console.log(path.resolve(__dirname, '..', 'public', 'assets' , 'images' , 'icons' , 'tecsomlogowhite.ico'));
 
     window.maximize();
 
@@ -173,12 +244,5 @@ const createMainWindow = async (app) => {
     return { browser: browser, window: window };
 
 }
-
-function containsOnlyNumbers(str) {
-
-    return /^\d+$/.test(str);
-
-}
-
 
 module.exports = { createApp };
